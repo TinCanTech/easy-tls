0d0a331c57a5dc74f42f12581cf05d0f40651118
Richard Bonhomme <tincanteksup@gmail.com>
2020-10-09 23:34:56 +0100

    Travis CI: Disable safety mode
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

--- easytls-op-test.sh
+++ easytls-op-test.sh
@@ -5,32 +5,47 @@
 
 
 CURL_TARGET="https://raw.githubusercontent.com/OpenVPN/easy-rsa/master/easyrsa3/easyrsa"
-curl -O "$CURL_TARGET"
+curl -O "$CURL_TARGET" || exit 77
 CURL_TARGET="https://raw.githubusercontent.com/OpenVPN/easy-rsa/master/easyrsa3/openssl-easyrsa.cnf"
-curl -O "$CURL_TARGET"
+curl -O "$CURL_TARGET" || exit 77
 CURL_TARGET="https://raw.githubusercontent.com/TinCanTech/Prebuilt-OpenVPN/master/src/openvpn/openvpn"
-curl -O "$CURL_TARGET"
+curl -O "$CURL_TARGET" || exit 77
 echo
 
 EXIT_CODE=1
-if [ -e "easyrsa" ];
+
+Required_file="./easyrsa"
+if [ -e "$Required_file" ];
 then
 	# '-e' lol
-	chmod 744 "easyrsa"
-	chmod 744 "openvpn"
-	EXIT_CODE=0
+	chmod 744 "$Required_file"
+else
+	echo "Failed to DL $Required_file"
+	exit 71
+fi
+
+Required_file="./openssl-easyrsa.cnf"
+if [ -e "$Required_file" ];
+then
+	# '-e' lol
+	chmod 744 "$Required_file"
+else
+	echo "Failed to DL $Required_file"
+	exit 72
+fi
+
+Required_file="./openvpn"
+if [ -e "$Required_file" ];
+then
+	# '-e' lol
+	chmod 744 "$Required_file"
+else
+	echo "Failed to DL $Required_file"
+	exit 73
+fi
+
 	export EASYTLS_OPENVPN="./openvpn"
 	printf "%s\n" "EASYTLS_OPENVPN=$EASYTLS_OPENVPN"
 	$EASYTLS_OPENVPN --version
-	time sh easytls-unit-tests.sh || EXIT_CODE=1
-else
-	echo "Failed to DL easyrsa"
-fi
 
-echo
-echo "exit code: $EXIT_CODE"
-echo "Over ride failure!"
-echo "This is all tested locally prior to release"
-echo "Travis CI is used to provide open results"
-EXIT_CODE=0
-exit 0
+	time sh easytls-unit-tests.sh
