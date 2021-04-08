16420cf86b846cc7046adcfbf071429a836699e3
Richard Bonhomme <tincanteksup@gmail.com>
2021-04-09 00:11:05 +0100

    Add easytls-verify.bat (--tls-verify for Windows)
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

---
+++ easytls-verify.bat
@@ -0,0 +1,47 @@
+@echo OFF
+REM Easy-TLS script launcher
+REM Automatically set PATH to Easy-RSA program-files
+FOR /F "tokens=2*" %%a IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\OpenVPN" /ve') DO set BASE_PATH=%%b
+PATH=%BASE_PATH%easy-rsa\bin;%BASE_PATH%bin;%PATH%
+
+REM This MUST CD to your Easy-RSA v3 working application directory
+REM Please use 8.3 names without spaces
+SET WORK_DIR=/home/tct/git/tct/FULL_TEST
+CD %WORK_DIR%
+
+SET LOG=%WORK_DIR%/easytls-verify.log
+SET SH_EXIT=9
+
+SET VERB=-v
+SET CA_DIR=/home/tct/git/tct/FULL_TEST/pki
+SET PID_FILE=-s=%WORK_DIR%/easytls-server.pid
+SET TMP_DIR=-t=%WORK_DIR%
+
+ECHO * easytls-verify.bat * > %LOG%
+REM Run the script
+REM -c|--ca-path can be absolute or relative to WORK_DIR
+sh.exe easytls-verify.sh %VERB% -c=%CA_DIR% %TMP_DIR% %PID_FILE% >> %LOG% 2>&1
+
+REM AOK
+IF ERRORLEVEL 0 SET SH_EXIT=0
+REM X509 certificate unknown
+IF ERRORLEVEL 1 SET SH_EXIT=1
+REM CA PKI dir not defined. (REQUIRED)
+IF ERRORLEVEL 2 SET SH_EXIT=2
+REM CA cert not found
+IF ERRORLEVEL 3 SET SH_EXIT=3
+REM index.txt not found
+IF ERRORLEVEL 4 SET SH_EXIT=4
+REM Server PID file has not been configured
+IF ERRORLEVEL 5 SET SH_EXIT=5
+REM Server PID does not match daemon_pid
+IF ERRORLEVEL 6 SET SH_EXIT=6
+REM missing value to optionmissing value to option
+IF ERRORLEVEL 6 SET SH_EXIT=7
+REM missing X509 client cert serial
+IF ERRORLEVEL 6 SET SH_EXIT=8
+REM other error
+IF ERRORLEVEL 9 SET SH_EXIT=9
+
+ECHO SH_EXIT: %SH_EXIT% >> %LOG%
+EXIT /B %SH_EXIT%
