629d3517b9dad846d644cc50857250a5f3107ad1
Richard Bonhomme <tincanteksup@gmail.com>
2021-04-25 22:13:00 +0100

    Add github workflow for Windows
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

---
+++ easytls-op-test.bat
@@ -0,0 +1,14 @@
+@echo OFF
+
+if %PROCESSOR_ARCHITECTURE% == x86 (
+    curl -LO https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8-win32.zip
+    7z e EasyRSA-3.0.8-win32.zip
+) ELSE (
+    curl -LO https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8-win64.zip
+    7z e EasyRSA-3.0.8-win64.zip
+)
+
+curl -LO https://raw.githubusercontent.com/TinCanTech/easyrsa-plus/master/easyrsa3/easyrsa
+
+SET SHALLOW=1
+sh.exe easytls-unit-tests.sh
