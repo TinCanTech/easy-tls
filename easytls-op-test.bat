4254163962be76127592bf6103d79a7442db0350
Richard Bonhomme <tincanteksup@gmail.com>
2021-04-26 14:28:32 +0100

    Add 64b downloads
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

--- easytls-op-test.bat
+++ easytls-op-test.bat
@@ -6,6 +6,8 @@ if %PROCESSOR_ARCHITECTURE% == x86 (
 ) ELSE (
     curl -LO https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8-win64.zip
     7z e EasyRSA-3.0.8-win64.zip
+    curl -LO https://github.com/TinCanTech/Prebuilt-Openvpn/blob/master/wovpn/wovpn64b.zip
+    7z e wovpn64b.zip
 )
 
 curl -LO https://raw.githubusercontent.com/TinCanTech/easyrsa-plus/master/easyrsa3/easyrsa
