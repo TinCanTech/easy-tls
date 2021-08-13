c0ace6c6740315407776bef1b3b6a4827be36f84
Richard T Bonhomme <tincantech@protonmail.com>
2021-08-13 18:09:45 +0100

    shellcheck minor tweaks
    Signed-off-by: Richard T Bonhomme <tincantech@protonmail.com>

--- easytls-shellcheck.sh
+++ easytls-shellcheck.sh
@@ -13,39 +13,39 @@ export SHELLCHECK_OPTS="--shell=sh -S warning -e 1090 $*"
 # SC1090 - Can't follow non-constant source
 # Recommend -e 2034
 
-foo='========================='
+foo='========================================================================'
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls'
 "${shellcheck_bin}" easytls && sc_easytls=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-cryptv2-verify.sh'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-cryptv2-verify.sh'
 "${shellcheck_bin}" easytls-cryptv2-verify.sh && sc_easytls_cryptv2_verify=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-cryptv2-verify.vars'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-cryptv2-verify.vars'
 "${shellcheck_bin}" easytls-cryptv2-verify.vars && sc_easytls_cryptv2_verify_vars=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-verify.sh'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-verify.sh'
 "${shellcheck_bin}" easytls-verify.sh && sc_easytls_verify=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-verify.vars'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-verify.vars'
 "${shellcheck_bin}" easytls-verify.vars && sc_easytls_verify_vars=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-connect.sh'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-connect.sh'
 "${shellcheck_bin}" easytls-client-connect.sh && sc_easytls_client_connect=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-connect.vars'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-connect.vars'
 "${shellcheck_bin}" easytls-client-connect.vars && sc_easytls_client_connect_vars=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-disconnect.sh'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-disconnect.sh'
 "${shellcheck_bin}" easytls-client-disconnect.sh && sc_easytls_client_disconnect=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-disconnect.vars'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-disconnect.vars'
 "${shellcheck_bin}" easytls-client-disconnect.vars && sc_easytls_client_disconnect_vars=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-disconnect.sh'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-client-disconnect.sh'
 "${shellcheck_bin}" easytls-conn-trac.lib && sc_easytls_conn_trac=$?
 
-printf '\n\n%s\n%s\n' "$foo" '*** shellcheck easytls-shellcheck.sh'
+printf '\n%s\n%s\n' "$foo" '*** shellcheck easytls-shellcheck.sh'
 "${shellcheck_bin}" easytls-shellcheck.sh && sc_easytls_shellcheck=$?
 
 exit_status=$(( \
@@ -61,6 +61,9 @@ exit_status=$(( \
 					sc_easytls_conn_trac + \
 					sc_easytls_shellcheck \
 			 ))
+printf '\n'
 
 # dirty trick to fool my CI and still record a fail
+# IMHO, shellcheck should check for this but does not ...
+#[ $exit_status -gt 0 ] && echo "Easy-TLS Shellcheck exit status: $exit_status"
 [ $exit_status -eq 0 ] || echo "Easy-TLS Shellcheck exit status: $exit_status"
