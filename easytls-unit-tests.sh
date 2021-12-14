922f7ab9faee35a2dba588e2311ea875f3925e11
Richard T Bonhomme <tincantech@protonmail.com>
2021-12-14 19:26:33 +0000

    Drop TLS-Crypt-V2 GROUP key 'type-label' from name
    The type-labels were: '-grp-srv' Server and '-grp-cli' Client.
    These labels force a Server group key to be built before a Client
    group key can be built.  This is totally inflexible.
    
    By dropping these 'type-labels' it is possible to build a Client
    group key from an existing Server key.  So Groups can be added to
    standard Server keys.
    
    Signed-off-by: Richard T Bonhomme <tincantech@protonmail.com>

--- easytls-unit-tests.sh
+++ easytls-unit-tests.sh
@@ -212,7 +212,7 @@ for cmd in "init no-ca" "cf cg easytls-unit-test" \
 			"-r=s03 itcv2 c05" "-r=s03 -k=hw itcv2 c05 no-md" \
 			"-r=s03 itcv2 c06" "-r=s03 -k=hw itcv2 c06 add-hw" \
 			"-k=hw rmd c06 serial" "status" \
-			"bc2gs g01" "bc2gc g01 g01" "bc2gc g01 g02 ${hwaddr1} ${hwaddr2} ${ip4addr} ${ip6addr}"
+			"bc2gs sg01" "bc2gc sg01 cg01" "bc2gc sg01 cg02 ${hwaddr1} ${hwaddr2} ${ip4addr} ${ip6addr}"
 do
 	[ "${cmd}" = 99 ] && exit 99
 	echo "--------------------"
@@ -359,8 +359,8 @@ do
 		"--custom-group=tincantech --sub-key-name=eve remove-tlskey c10" \
 		"cert-expire" \
 		"inline-expire" \
-		"bc2gs g01" "bc2gc g01 g01" "bc2gc g01 g02 ${hwaddr1} ${hwaddr2} ${ip4addr} ${ip6addr}" \
-		"ic2gs s01 g01" "ic2gc c01 g01" "ic2gc c01 g02"
+		"bc2gs sg01" "bc2gc sg01 cg01" "bc2gc sg01 cg02 ${hwaddr1} ${hwaddr2} ${ip4addr} ${ip6addr}" \
+		"ic2gs s01 sg01" "ic2gc c01 cg01" "ic2gc c01 cg02"
 		#"inline-index-rebuild" \
 		## EOL
 	do
