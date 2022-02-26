2f72141ffa697d1e02745ef48a93790d574ca54a
Richard T Bonhomme <tincantech@protonmail.com>
2022-02-26 01:09:03 +0000

    Fix unit-test
    Signed-off-by: Richard T Bonhomme <tincantech@protonmail.com>

--- dev/easytls-op-test.sh
+++ dev/easytls-op-test.sh
@@ -58,6 +58,14 @@ for f in ${etls_ut_file_list}; do
 	cp -v "./${f}" "${etls_ut_dir_name}/${f}"
 done
 
+mkdir -p 	"${etls_ut_dir_name}/dev/et-tdir1" \
+			"${etls_ut_dir_name}/dev/et-tdir2" \
+			"${etls_ut_dir_name}/dev/et-tdir3"
+
+cp -Rv ./dev/et-tdir1/* "${etls_ut_dir_name}/dev/et-tdir1"
+cp -Rv ./dev/et-tdir2/* "${etls_ut_dir_name}/dev/et-tdir2"
+cp -Rv ./dev/et-tdir3/* "${etls_ut_dir_name}/dev/et-tdir3"
+
 cd "${etls_ut_dir_name}"
 
 dl_ossl3
