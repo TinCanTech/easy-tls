cbfc6f1c1dbff834d92b510f5d310599394f3d3b
Richard T Bonhomme <tincantech@protonmail.com>
2023-08-23 22:24:56 +0100

    unit test: Use easyrsa make-safe-ssl not makesafessl
    Signed-off-by: Richard T Bonhomme <tincantech@protonmail.com>

--- dev/easytls-unit-tests.sh
+++ dev/easytls-unit-tests.sh
@@ -615,7 +615,7 @@ elif [ -n "${EASYTLS_REMOTE_CI}" ]; then
 	# Update safessl-easyrsa.cnf
 	print "*** Update safessl-easyrsa.cnf - easyrsa init-pki"
 	"${EASYRSA_CMD}" --pki-dir="${WORK_DIR}/et-tdir${loops}" --batch init-pki
-	"${EASYRSA_CMD}" --pki-dir="${WORK_DIR}/et-tdir${loops}" --batch makesafessl
+	"${EASYRSA_CMD}" --pki-dir="${WORK_DIR}/et-tdir${loops}" --batch make-safe-ssl
 
 	cp -vf "${WORK_DIR}/et-tdir${loops}/safessl-easyrsa.cnf" ./safessl-easyrsa.cnf || \
 		fail "(1) cp ${WORK_DIR}/et-tdir${loops}/safessl-easyrsa.cnf ./safessl-easyrsa.cnf"
